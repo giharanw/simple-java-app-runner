8048cb96f74c755455db4e9bad9c26818f0a43f5
Gihara Wicktramatunge <giharanw@gmail.com>
2022-02-25 00:50:46 +0530

    Implement btnRun part - 2

--- src/SimpleJavaAppRunnerFormController.java
+++ src/SimpleJavaAppRunnerFormController.java
@@ -3,6 +3,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SimpleJavaAppRunnerFormController {
 
     public AnchorPane root;
@@ -10,13 +16,17 @@ public class SimpleJavaAppRunnerFormController {
     public TextArea txtOutput;
     public Button btnRun;
 
-    public void btnRunOnAction(ActionEvent actionEvent) {
+    public void btnRunOnAction(ActionEvent actionEvent) throws IOException {
 
         String data =   "public class App{" +
                             "public static void main(String[] args{" +
                                 txtCode.getText()+
                             "})"+
                         "}";
+
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        Path tmpFilePath = Paths.get(tmpDir,"App.java");
+        Files.write(tmpFilePath,data.getBytes());
 
     }
 
