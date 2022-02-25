e24f848e43a1eaea76054879fdc15d24a0c518f0
Gihara Wicktramatunge <giharanw@gmail.com>
2022-02-25 07:20:34 +0530

    Implement btnRun part - 4

--- src/SimpleJavaAppRunnerFormController.java
+++ src/SimpleJavaAppRunnerFormController.java
@@ -4,6 +4,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,23 +20,40 @@ public class SimpleJavaAppRunnerFormController {
     public void btnRunOnAction(ActionEvent actionEvent) throws IOException, InterruptedException {
 
         String data =   "public class App{" +
-                            "public static void main(String[] args{" +
+                            "public static void main(String[] args){" +
                                 txtCode.getText()+
-                            "})"+
+                            "}"+
                         "}";
 
         String tmpDir = System.getProperty("java.io.tmpdir");
         Path tmpFilePath = Paths.get(tmpDir,"App.java");
         Files.write(tmpFilePath,data.getBytes());
 
-        Process javac = Runtime.getRuntime().exec("javac"+tmpFilePath);
+        Process javac = Runtime.getRuntime().exec("javac "+tmpFilePath);
         int exitCode = javac.waitFor();
+        System.out.println("javac");
 
         if (exitCode==0) {
+            Process java = Runtime.getRuntime().exec("java -cp " + tmpDir + " App");
+            exitCode = java.waitFor();
+            System.out.println("java");
 
+            if (exitCode==0){
+                InputStream is = java.getInputStream();
+                byte[] buffer = new byte[is.available()];
+                is.read(buffer);
+                txtOutput.setText(new String(buffer));
+                is.close();
+            }
+            else {
+                /* Todo: Handle this later */
+                System.out.println("Run time error!");
+
+            }
         }
         else {
             /* Todo: Handle this later */
+            System.out.println("Compile time error!");
         }
 
     }
