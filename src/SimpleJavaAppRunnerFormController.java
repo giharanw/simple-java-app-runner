941b8c12bdb569c3da4b7df21377eba5459babff
Gihara Wicktramatunge <giharanw@gmail.com>
2022-02-25 07:47:11 +0530

    Implement btnRun part - 5

--- src/SimpleJavaAppRunnerFormController.java
+++ src/SimpleJavaAppRunnerFormController.java
@@ -17,45 +17,48 @@ public class SimpleJavaAppRunnerFormController {
     public TextArea txtOutput;
     public Button btnRun;
 
-    public void btnRunOnAction(ActionEvent actionEvent) throws IOException, InterruptedException {
+    public void btnRunOnAction(ActionEvent actionEvent) throws IOException {
 
-        String data =   "public class App{" +
-                            "public static void main(String[] args){" +
-                                txtCode.getText()+
-                            "}"+
-                        "}";
+        try {
+            String data = "public class App{" +
+                    "public static void main(String[] args){" +
+                    txtCode.getText() +
+                    "}" +
+                    "}";
 
-        String tmpDir = System.getProperty("java.io.tmpdir");
-        Path tmpFilePath = Paths.get(tmpDir,"App.java");
-        Files.write(tmpFilePath,data.getBytes());
+            String tmpDir = System.getProperty("java.io.tmpdir");
+            Path tmpFilePath = Paths.get(tmpDir, "App.java");
+            Files.write(tmpFilePath, data.getBytes());
 
-        Process javac = Runtime.getRuntime().exec("javac "+tmpFilePath);
-        int exitCode = javac.waitFor();
-        System.out.println("javac");
+            Process javac = Runtime.getRuntime().exec("javac " + tmpFilePath);
+            int exitCode = javac.waitFor();
 
-        if (exitCode==0) {
-            Process java = Runtime.getRuntime().exec("java -cp " + tmpDir + " App");
-            exitCode = java.waitFor();
-            System.out.println("java");
+            if (exitCode == 0) {
+                Process java = Runtime.getRuntime().exec("java -cp " + tmpDir + " App");
+                exitCode = java.waitFor();
 
-            if (exitCode==0){
-                InputStream is = java.getInputStream();
-                byte[] buffer = new byte[is.available()];
-                is.read(buffer);
-                txtOutput.setText(new String(buffer));
-                is.close();
+                if (exitCode == 0) {
+                    readStream(java.getInputStream());
+                } else {
+                    readStream(java.getErrorStream());
+                }
+            } else {
+                readStream(javac.getErrorStream());
             }
-            else {
-                /* Todo: Handle this later */
-                System.out.println("Run time error!");
-
-            }
+        } catch (IOException | InterruptedException e){
+            e.printStackTrace();
+        } finally {
+            /* Deleting java and class files from temp directory */
+            Files.deleteIfExists(Paths.get(System.getProperty("java.io.tmpdir") ,"App.java"));
+            Files.deleteIfExists(Paths.get(System.getProperty("java.io.tmpdir") , "App.class"));
         }
-        else {
-            /* Todo: Handle this later */
-            System.out.println("Compile time error!");
-        }
+    }
 
+    private void readStream(InputStream is) throws IOException {
+        byte[] buffer = new byte[is.available()];
+        is.read(buffer);
+        txtOutput.setText(new String(buffer));
+        is.close();
     }
 
 }
